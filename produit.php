6b3f8c785c0f40f38f00e7b8aa4780f6e52e7847
N'DRI KOUAME JEREMIE <ndrikouamejeremie@gmail.com>
2025-11-20 11:53:40 +0000

    premiere version

---
+++ produit.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/styles/index.css">
+  <title>Nos produits</title>
+</head>
+
+<body>
+  <?php include('menu.php'); ?>
+  <div class="home-page__background_produit">
+    <h1 class="titre"><strong>NOUS AVONS DES PRODUITS DE QUALITES</strong></h1>
+    <!-- <img src="" alt="Image de fond" class="home-page__image" class="produit-image"> -->
+  </div>
+  <section class="propos-page">
+    <div class="equipes">
+      <div class="cart-produit">
+        <img src="assets/images/image/produit1.jpg" alt="Produit 1" class="produit-image">
+        <div class="produit-details">
+          <h2>Beurre de Cacao</h2>
+          <p>
+            Notre beurre de cacao est fabriqué à partir des fèves de cacao les plus fines,
+            offrant une qualité exceptionnelle pour vos produits cosmétiques et culinaires.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="equipes">
+      <div class="cart-produit">
+        <img src="assets/images/image/produit1.jpg" alt="Produit 1" class="produit-image">
+        <div class="produit-details">
+          <h2>Beurre de Cacao</h2>
+          <p>
+            Notre beurre de cacao est fabriqué à partir des fèves de cacao les plus fines,
+            offrant une qualité exceptionnelle pour vos produits cosmétiques et culinaires.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <?php include 'footer.php'; ?>
+</body>
+
+</html>
